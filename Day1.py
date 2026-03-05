36c2e52315b21c6994295fa55e90be46ae23131f
Muhammad Ahmad Malahim <malahim0124@gmail.com>
2026-03-05 19:14:54 +0500

    Day 1 of Python

---
+++ Day1.py
@@ -0,0 +1,9 @@
+# Day 1 — Smart Greeting Program
+
+name = input("Enter Your Name: ") # getting name for greeting
+
+age = int(input("Enter Your Age: ")) # getting age fo age calculation
+
+age_calculation = 50 - age # age calculation
+
+print(f"Hi {name},\nYou have {age_calculation} years left to reach the age of 50.") # Greeting Output
