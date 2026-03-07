f7d0387c674562ddb559262e50235e1be14e8104
Muhammad Ahmad Malahim <malahim0124@gmail.com>
2026-03-07 20:05:55 +0500

    Even / Odd Checker

---
+++ Day_3.py
@@ -0,0 +1,8 @@
+# Day 3 - Even / Odd Checker
+number = int(input("Enter Number: "))
+
+if number % 2 == 0: # opertion for Even Number
+    print(f"{number} is Even") # Show Output if Number is Even
+
+elif number % 2 != 0: # Opertion for Odd Number
+    print(f"{number} is Odd") # Show Output if Number is Odd
