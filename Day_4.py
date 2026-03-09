d7f1aa8da50f5913dd294b160ff7a7b2ec8a851a
Muhammad Ahmad Malahim <malahim0124@gmail.com>
2026-03-09 11:11:18 +0500

    Day 4 Parctice Done

---
+++ Day_4.py
@@ -0,0 +1,14 @@
+# Day 4 - Number Guessing Game
+
+import random #random module
+number = random.randint(1,10)
+guess = 0
+while guess != number:
+    guess = int(input("Guess a Number Between 1 to 10: "))
+
+    if guess == number:
+        print("Correct! You guessed the number.")
+    elif guess > number:
+        print("Too high! Try again.")
+    else:
+        print("Too low! Try again.")
