c5542b4ebb324bc53d645245de142fd2bd122f0b
Muhammad Ahmad Malahim <malahim0124@gmail.com>
2026-03-09 11:16:49 +0500

    Number Guessing Game

--- Day_4.py
+++ Day_4.py
@@ -3,10 +3,11 @@
 import random #random module
 number = random.randint(1,10)
 guess = 0
-while guess != number:
-    guess = int(input("Guess a Number Between 1 to 10: "))
-
-    if guess == number:
+while guess != number: # While Loop for Iteration
+    guess = int(input("Guess a Number Between 1 to 10: ")) # Take input from user
+    
+    # Conditions
+    if guess == number: 
         print("Correct! You guessed the number.")
     elif guess > number:
         print("Too high! Try again.")
