665023336a5ea9cbe4b643c33c2334da38541ed3
Muhammad Ahmad Malahim <malahim0124@gmail.com>
2026-04-08 21:48:52 +0500

    NumPy Array Basics

---
+++ Day_11.py
@@ -0,0 +1,15 @@
+# Day 11 — NumPy Array Basics
+
+import numpy as np
+
+# Create NumPy array
+arr = np.array([10, 20, 30, 40])
+
+# Print array
+print("Array:", arr)
+
+# Print shape
+print("Shape:", arr.shape)
+
+# Print data type
+print("Data Type:", arr.dtype)
