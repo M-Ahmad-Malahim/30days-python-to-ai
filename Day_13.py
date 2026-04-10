240eb46874cdf628dde9c7b6cf6a9320d3941af4
Muhammad Ahmad Malahim <malahim0124@gmail.com>
2026-04-10 19:26:27 +0500

    Random Data Generator

---
+++ Day_13.py
@@ -0,0 +1,9 @@
+# Day 13 — Random Data Generator
+
+import numpy as np
+
+# Generate 10 random numbers between 1 and 100
+random_data = np.random.randint(1, 101, 10)
+
+# Display result
+print("Random Data:", random_data)
